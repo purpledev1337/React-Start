fbee55aaf335ae31ffa5a7d80f4ddfa44b5047a9
Francesco Tappi <france.tappi@gmail.com>
2022-12-15 20:21:07 +0100

    ExpenseDate: Splitting ExpenseItem component into multiple components. Added props and style.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,11 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
 
-    const month = props.date.toLocaleString('en-EU', { month: 'long' });
-    const day = props.date.toLocaleString('en-EU', { day: '2-digit' });
-    const year = props.date.getFullYear();
-
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div className="expense-item__day">{day}</div>
-                <div>{year}</div>
-            </div>
+            <ExpenseDate date={props.date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">€ {props.amount}</div>
