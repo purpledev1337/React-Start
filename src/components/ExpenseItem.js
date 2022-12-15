fe67fc5744532660a896555c9f4d9b57ddd7dc2e
Francesco Tappi <france.tappi@gmail.com>
2022-12-15 19:55:23 +0100

    ExpenseItem: added dummy content and css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,16 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>;
+
+    return (
+        <div className="expense-item">
+            <div>April 23th 2022</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$314.96</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
