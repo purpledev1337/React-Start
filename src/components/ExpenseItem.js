b0cf4b7c2c8adf7f9078a8dbb6b0bf456671694e
Francesco Tappi <france.tappi@gmail.com>
2022-12-15 20:02:50 +0100

    ExpenseItem: added dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,12 +2,16 @@ import './ExpenseItem.css';
 
 function ExpenseItem() {
 
+    const expenseDate = new Date(2022, 4, 23);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 314.96;
+
     return (
         <div className="expense-item">
-            <div>April 23th 2022</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$314.96</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">{expenseAmount}</div>
             </div>
         </div>
     );
