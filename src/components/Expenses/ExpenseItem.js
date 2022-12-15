399d5dff6373a24797d8cf0a7d9d54c36a11d9ea
Francesco Tappi <france.tappi@gmail.com>
2022-12-15 23:20:17 +0100

    ExpenseItem: useState for update the title with button click handler function

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,24 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
 
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle('Updated!');
+        console.log(title);
+    };
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
+                <button onClick={clickHandler}>Change Title</button>
                 <div className="expense-item__price">€ {props.amount}</div>
             </div>
         </Card>
