a1f566691ab3562185241ffd2ae64678d7d5e4f3
Francesco Tappi <france.tappi@gmail.com>
2022-12-16 00:14:31 +0100

    NewExpense: Child to Parent Communication

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
-function ExpenseForm() {
+function ExpenseForm(props) {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -55,7 +55,8 @@ function ExpenseForm() {
       date: new Date(enteredDate),
     };
 
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
+
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
--- src/components/Expenses/NewExpense.js
+++ src/components/Expenses/NewExpense.js
@@ -2,9 +2,14 @@ import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 function NewExpence() {
+    function onSaveExpenseDataHandler(enteredExpenseData) {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
     return (
         <div className="new-expense">
-            <ExpenseForm />
+            <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
         </div>
     );
 }
