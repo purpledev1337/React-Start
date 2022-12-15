1f285373f9cad7f07d022322e27ea005247870d4
Francesco Tappi <france.tappi@gmail.com>
2022-12-16 00:47:50 +0100

    Expenses: Added ExpensesFilter as child and make him communicate with parent on dropdown change.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,18 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+
+    function readFilterValue(value) {
+        
+        console.log(value);
+    };
+
     return (
         <Card className="expenses">
+            <ExpensesFilter onDropDownChange={readFilterValue} />
             {props.items.map((expense) => {
                 return <ExpenseItem
                     key={expense.id}
