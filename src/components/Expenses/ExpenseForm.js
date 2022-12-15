d98ff6f7da6fcaf4a458b378d51b9476158741c7
Francesco Tappi <france.tappi@gmail.com>
2022-12-15 23:46:24 +0100

    ExpenseForm: more correct wat to avoid issues. (use prevState to update the state)

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -11,23 +11,32 @@ function ExpenseForm() {
     });
 
     function titleChangeHandler(event) {
-        setUserInput({
-            ...userInput,
-            enteredTitle: event.target.value
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value
+        // });
+        setUserInput((prevState) => {
+            return { ...prevState, enteredTitle: event.target.value }
         });
     };
 
     function amountChangeHandler(event) {
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.target.value
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value
+        // });
+        setUserInput((prevState) => {
+            return { ...prevState, enteredAmount: event.target.value }
         });
     };
 
     function dateChangeHandler(event) {
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value
+        // });
+        setUserInput((prevState) => {
+            return { ...prevState, enteredDate: event.target.value }
         });
     };
 
