a16096ef112ab3a8359106e8ccff31706648dc65
purpleDev1337 <france.tappi@gmail.com>
2022-12-17 23:58:25 +0100

    Expenses: Added conditional Content if filtered array is empty. Commented sugar syntax.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -21,14 +21,17 @@ function Expenses(props) {
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-            {filteredExpenses.map((expense) => {
+            {/* {filteredExpenses.length === 0 && <p>No expenses found.</p>} */}
+            {filteredExpenses.length === 0 ? (<p>No expenses found.</p>) : (
+
+            filteredExpenses.map((expense) => {
                 return <ExpenseItem
                     key={expense.id}
                     title={expense.title}
                     amount={expense.amount}
                     date={expense.date}>
                 </ExpenseItem>
-            })}
+            }))}
         </Card>
     )
 }
