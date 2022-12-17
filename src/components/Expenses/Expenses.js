8af7d04330c06ff7f395809222a6390ca53ebabb
purpleDev1337 <france.tappi@gmail.com>
2022-12-17 23:49:54 +0100

    Expenses: added filtered array by selected year.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,10 +14,14 @@ function Expenses(props) {
         setFilteredYear(selectedYear);
     };
 
+    const filteredExpenses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-            {props.items.map((expense) => {
+            {filteredExpenses.map((expense) => {
                 return <ExpenseItem
                     key={expense.id}
                     title={expense.title}
